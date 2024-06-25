58f73b1e3cba1dea7232057d3950ba564a70b650
SriramKalki <sriramkalki@hotmail.com>
2024-06-24 20:31:55 -0500

    Delete functionality

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
 // Initialize upload
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000 }, // 1MB limit
+  limits: { fileSize: 30000000 }, // 30MB limit
   fileFilter: (req, file, cb) => {
     checkFileType(file, cb);
   }
@@ -52,7 +52,37 @@ app.post('/upload', (req, res) => {
   });
 });
 
+app.get('/uploads/:filename', (req, res) => {
+  const filePath = path.join(__dirname, 'uploads', req.params.filename);
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.status(404).send('File not found');
+    } else {
+      res.sendFile(filePath);
+    }
+  });
+});
 
+app.get('/files', (req, res) => {
+  fs.readdir('./uploads', (err, files) => {
+    if (err) {
+      res.status(500).json({ message: 'Unable to retrieve files' });
+    } else {
+      res.json(files);
+    }
+  });
+});
+
+app.delete('/uploads/:filename', (req, res) => {
+    const filePath = path.join(__dirname, 'uploads', req.params.filename);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        res.status(404).send('File not found');
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  });
+  
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
